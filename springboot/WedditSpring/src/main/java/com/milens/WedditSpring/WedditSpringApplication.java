4d26130a7ae8b5e17f4469b1e61b8f96c157df7f
miki2017milan <milanmilenkovic20@gmail.com>
2025-06-04 21:28:01 +0200

    added testing springboot project

---
+++ springboot/WedditSpring/src/main/java/com/milens/WedditSpring/WedditSpringApplication.java
@@ -0,0 +1,13 @@
+package com.milens.WedditSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedditSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WedditSpringApplication.class, args);
+	}
+
+}
---
+++ springboot/WedditSpring/src/test/java/com/milens/WedditSpring/WedditSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milens.WedditSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WedditSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
